5a3778c2b8a54ea0843b50ac9e380cbe88ffe8cb
cyh42 <cyh42@outlook.com>
2020-01-31 17:39:41 +0800

    add photos

--- tool.py
+++ tool.py
@@ -191,9 +191,13 @@ def git_operation():
 
 if __name__ == "__main__":
     cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
+    print('1')
     compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
+    print('2')
     git_operation()    # 提交到github仓库
+    print('3')
     handle_photo()     # 将文件处理成json格式，存到博客仓库中
+    print('4')
     
     
     
